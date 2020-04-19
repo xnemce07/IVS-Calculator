c3e5e591bf3730606da198193f0c024eaf5c2fd6
xhyksa00 <xhyksa00@stud.fit.vutbr.cz>
2020-04-19 18:22:35 +0200

    upravy matematickej kniznice

--- CustomMath/CustomMath.cpp
+++ CustomMath/CustomMath.cpp
@@ -1,108 +1,138 @@
+#include "pch.h"
 #include "CustomMath.h"
 #include <stdlib.h>
 
-#define eps 0.0000000001
-
 /******************************************/
 
-double Add(double a, double b) {
-        return a + b;
+double CustomMath::addition(double a, double b) {
+    return a + b;
 }
 
-double Sub(double a, double b) {
-        return a - b;
+double CustomMath::substraction(double a, double b) {
+    return a - b;
 }
 
-double Mult(double a, double b) {
-        return a * b;
+double CustomMath::multiplication(double a, double b) {
+    return a * b;
 }
 
-double Div(double a, double b) {
-        return a / b;
-}
-
-int Fact(int n) {
-        if((n == 1) || (n == 0))
-                return n;
-        else
-                return n * factorial(n - 1);
-}
-
-double Pow(double a, int n) {
-        if(n == 0)
-                return 1;
-        else
-                if(n == 1)
-                        return a;
-                else
-                        return a * power(a, n - 1);
-}
-
-double Root(double a, int n) {
-        if((a < 0) && !(n % 2))
-                return NULL;
-        bool neg = 0;
-        if(a < 0) {
-                a *= -1;
-                neg = 1;
-        }
-
-        double low = 0;
-        double result = a / 2;
-        double high = a;
-
-        while(((power(result, n) - a) !< eps) || ((a - power(result, n)) !< eps)) {
-                if(power(result, n) < a) 
-                        low = result;
-                else
-                        high = result;
-                result = (low + high) / 2;
-        }
-
-	if(neg)
-		result *= -1;
-
-        return result;
-}
-
-double Sin(double cislo)
+double CustomMath::division(double dividend, double divisor)
 {
-	cislo = toPi(cislo);
-	int poradie = 1;
-	int klad = 2;
-	double hodnota = 0.0;
-	double clen = 0;
-
-	do {
-		clen = 1;
-
-		for(int i = 1; i <= poradie; i++) {
-			clen *= cislo;
-			clen /= (double)i;
-        	}
-
-		if (!(klad % 2))
-			hodnota += clen;
-		else
-			hodnota -= clen;
-
-		klad++;
-		poradie += 2;
-	} while(clen > eps);
-
-	return hodnota;
+    if (divisor == 0.0) {
+        throw "Math error: division by Zero!\n";
+    }
+    return (dividend / divisor);
 }
 
-double toPi(double a) {
-	if(a == 0)
-		return 0;
-	else
-		if(a < 0)
-			return toPi(a + 2 * M_PI);
-		else
-			if(a > 2 * M_PI)
-				return toPi(a - 2 * M_PI);
-	return a;
+double CustomMath::factorial(double n) {
+    if (n < 0) {
+        throw "Math error: factorial of negative/decimal number!\n";
+    }
+    if (n == 1 || n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
 }
 
+double CustomMath::power(double a, double n) {
+    double exp = floor(n);
+    if (n != exp) {
+        throw "Math error: power - expected integer as exponent!\n";
+    }
+    if (a == 0 && n < 0) {
+        throw "Math error: power - division by zero!\n";
+    }
+    if (n == 0)
+        return 1;
+    else
+        if (n == 1)
+            return a;
+        else if (n > 0)
+            return a * power(a, n - 1);
+        else {
+            a = 1 / a;
+            n *= -1;
+            return power(a, n);
+        }
+}
 
+double CustomMath::root(double a, double n) {
+    int degree = floor(n);
+    if (degree != n) {
+        throw "Math error: root - expected integer as degree!\n";
+    }
+    if ((a < 0) && !(degree % 2)) {
+        throw "Math error: even root of a negative number!\n";
+    }
+    if (degree == 0) {
+        throw "Math error: root - division by zero!\n";
+    }
+    if (a == 0 && degree == -1) {
+        throw "Math error: root - division by zero!\n";
+    }
+
+    bool neg = 0;
+    if (a < 0) {
+        a *= -1;
+        neg = 1;
+    }
+
+    double low = 0;
+    double result = a / 2;
+    double high = a;
+    double prev_result = a;
+
+    while (((prev_result - result) >= eps) || ((result - prev_result) >= eps)) {
+        if (power(result, degree) < a)
+            low = result;
+        else
+            high = result;
+        prev_result = result;
+        result = (low + high) / 2;
+    }
+
+    if (neg)
+        result *= -1;
+
+    return result;
+}
+
+double CustomMath::mySin(double cislo)
+{
+    cislo = toPi(cislo);
+    int poradie = 1;
+    int klad = 2;
+    double hodnota = 0.0;
+    double clen = 0;
+
+    do {
+        clen = 1;
+
+        for (int i = 1; i <= poradie; i++) {
+            clen *= cislo;
+            clen /= (double)i;
+        }
+
+        if (!(klad % 2))
+            hodnota += clen;
+        else
+            hodnota -= clen;
+
+        klad++;
+        poradie += 2;
+    } while (clen > eps);
+
+    return hodnota;
+}
+
+double CustomMath::toPi(double a) {
+    if (a == 0)
+        return 0;
+    else
+        if (a < 0)
+            return toPi(a + 2 * M_PI);
+        else
+            if (a > 2 * M_PI)
+                return toPi(a - 2 * M_PI);
+    return a;
+}
--- CustomMath/CustomMath.h
+++ CustomMath/CustomMath.h
@@ -4,25 +4,37 @@
 *@brief lib containing implemented math functions
 *
 ************************************************/
+#pragma once
 
-double addition(double a, double b);
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-double substraction(double a, double b);
-
-double multiplication(double a, double b);
-
-double division(double a, double b);
-
-int factorial(int n);
-
-double power(double a, int n);
-
-double root(double a, int n);
-
-double mySin(double cislo, double eps);
-
-double toPi(double a);
+#ifndef CUSTOMMATH_H_
+#define CUSTOMMATH_H_
 
 #define eps 0.0000000001
 
+class CustomMath
+{
+public:
+    double addition(double a, double b);
+
+    double substraction(double a, double b);
+
+    double multiplication(double a, double b);
+
+    double division(double a, double b);
+
+    double factorial(double n);
+
+    double power(double a, double n);
+
+    double root(double a, double n);
+
+    double mySin(double cislo);
+
+    double toPi(double a);
+};
+
+#endif//!CUSTOMMATH_H_
 /******************************************/
